fb8d7b52c3ba03ac043aac0aed520205ac1bebf5
aldyshap <aldiarakbar@gmail.comm>
2026-05-01 13:11:25 +0500

    fixed some bugs

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -48,7 +48,11 @@ app.add_middleware(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://127.0.0.1:5174", "http://localhost:5174", "http://localhost:8000", "http://127.0.0.1:8000", "http://localhost:5173", "https://ai-privacy-auditor.vercel.app"],
+    allow_origins=[
+        "http://localhost:5173", 
+        "https://ai-privacy-auditor.vercel.app", # Сенің фронтың
+        "https://ai-privacy-auditor.onrender.com" # Сенің бэкің
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
